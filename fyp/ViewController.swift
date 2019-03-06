81f09b3574a6204d5aa6b11912990d04fd9091e3
Scarlet <info_ng@me.com>
2019-03-06 19:42:35 +0800

    Initial Commit

---
+++ fyp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fyp
+//
+//  Created by Scarlet on 6/3/2019.
+//  Copyright © 2019 Scarlet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
